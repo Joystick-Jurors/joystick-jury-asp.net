106b7d4fb3312958470bc6c83d38d2314dc5bc2f
slaugaus <slaugaus@gmail.com>
2024-04-06 23:53:10 -0600

    Template

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace joystick_jury_asp.net.Pages
+{
+	public class PrivacyModel : PageModel
+	{
+		private readonly ILogger<PrivacyModel> _logger;
+
+		public PrivacyModel(ILogger<PrivacyModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+		}
+	}
+
+}
